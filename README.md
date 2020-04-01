2c1d7551f7fd8bda2e8be3fb03d777bea1ec5d0e
Sergey V. DUDANOV <sergey.dudanov@gmail.com>
2020-04-01 21:59:58 +0400

    First example added

---
+++ README.md
@@ -0,0 +1,3 @@
+## avr-dmbs
+
+Examples how use [CLUNET library](https://github.com/clunet/avr-dmbs-lib) for [DMBS](https://github.com/abcminiuser/dmbs) build system for AVR MCUs.
---
+++ simple-led-control/example.c
@@ -0,0 +1,44 @@
+#include <avr/io.h>
+#include "clunet.h"
+
+#define LED_PIN             PB3
+#define COMMAND_LED_OFF     0
+#define COMMAND_LED_ON      1
+
+/* Main receiving callback function. Executed in user non-interrupt context from clunet_task(). Must be non-static. */
+uint8_t
+clunet_receive_cb(clunet_message_t *message)
+{
+    /* ignore all broadcast messages */
+    if (message->dst_address == CLUNET_ADDRESS_BROADCAST)
+        return CLUNET_CALLBACK_RESULT_NORMAL;
+
+    switch (message->command)
+    {
+    case COMMAND_LED_OFF:       /* On */
+        PORTB &= ~_BV(LED_PIN);
+        break;
+    case COMMAND_LED_ON:        /* Off */
+        PORTB |= _BV(LED_PIN);
+        break;
+    default:                    /* Inverse */
+        /* This op work only on MCUs with PCIRQ. Use PORTB ^= _BV(LED_PIN) instead. */
+        PINB = _BV(LED_PIN);
+        break;
+    }
+
+    return CLUNET_CALLBACK_RESULT_NORMAL;
+}
+
+int
+main(void)
+{
+    DDRB |= _BV(LED_PIN);
+
+	clunet_init();
+
+	for ( ; ; )
+	{
+        clunet_task();
+	}
+}
